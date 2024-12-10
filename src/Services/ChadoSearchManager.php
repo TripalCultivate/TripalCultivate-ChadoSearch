9140c598c63456268dc5c559581f845b73314027
Lacey Sanderson <laceyannesanderson@gmail.com>
2024-12-10 18:31:52 +0000

    Add routing for chado search plugin instances.

--- src/Services/ChadoSearchManager.php
+++ src/Services/ChadoSearchManager.php
@@ -7,6 +7,7 @@ use Drupal\Core\Extension\ModuleHandlerInterface;
 use Drupal\Core\Plugin\DefaultPluginManager;
 use Drupal\chado_search\ChadoSearch\Annotation\ChadoSearch;
 use Drupal\chado_search\ChadoSearch\Interfaces\ChadoSearchInterface;
+use Symfony\Component\Routing\Route;
 
 /**
  * Manages ChadoSearch Plugin instances.
@@ -18,6 +19,15 @@ final class ChadoSearchManager extends DefaultPluginManager {
 
   /**
    * Constructs the ChadoSearchManager object.
+   *
+   * @param \Traversable $namespaces
+   *   An object that implements \Traversable which contains the root paths
+   *   keyed by the corresponding namespace to look for plugin implementations.
+   * @param \Drupal\Core\Cache\CacheBackendInterface $cache_backend
+   *   An instance of the cache backend which is used to cache plugin
+   *   definitions for performance reasons.
+   * @param \Drupal\Core\Extension\ModuleHandlerInterface $module_handler
+   *   The module handler service which is used to invoke the alter hook.
    */
   public function __construct(\Traversable $namespaces, CacheBackendInterface $cache_backend, ModuleHandlerInterface $module_handler) {
     parent::__construct(
@@ -29,6 +39,47 @@ final class ChadoSearchManager extends DefaultPluginManager {
     );
     $this->alterInfo('chado_search_info');
     $this->setCacheBackend($cache_backend, 'chado_search_plugins');
+  }
+
+  /**
+   * Registers routes for all the ChadoSearch plugin instances.
+   *
+   * @return \Symfony\Component\Routing\Route[]
+   *   An array of route objects.
+   */
+  public function routes() {
+    $routes = [];
+
+    // Get a list of all the ChadoSearch plugin instances.
+    $search_instances = $this->getDefinitions();
+
+    // For each instance... create a route object and add it to the list.
+    foreach ($search_instances as $search_definition) {
+      $route_name = 'chado_search.form.' . $search_definition['id'];
+      // The following route is equivalent to the YAML.
+      // ALLCAPS replaced by values from the ChadoSearch instances annotation.
+      // @code
+      // chado_search.form.ID:
+      //   path: URL_PATH
+      //   defaults:
+      //     _form: \Drupal\chado_search\Form\ChadoSearchForm
+      //     _title: TITLE
+      //   requirements:
+      //     _permission: PERMISSION[0]+PERMISSION[1]
+      // @endcode
+      $routes[$route_name] = new Route(
+        $search_definition['url_path'],
+        [
+          '_form' => '\Drupal\chado_search\Form\ChadoSearchForm',
+          '_title' => (string) $search_definition['title'],
+        ],
+        [
+          '_permission' => implode('+', $search_definition['permissions']),
+        ]
+      );
+    }
+
+    return $routes;
   }
 
 }
