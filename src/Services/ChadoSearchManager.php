d87c1d1f2c89b8b71b790f9869ff1c88abd1ac46
Lacey Sanderson <laceyannesanderson@gmail.com>
2024-12-10 17:09:11 +0000

    Move plugin manager to be a service since it will do more then just create an instance.

--- src/Services/ChadoSearchManager.php
+++ src/Services/ChadoSearchManager.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Drupal\chado_search\ChadoSearch\PluginManager;
+namespace Drupal\chado_search\Services;
 
 use Drupal\Core\Cache\CacheBackendInterface;
 use Drupal\Core\Extension\ModuleHandlerInterface;
@@ -9,12 +9,15 @@ use Drupal\chado_search\ChadoSearch\Annotation\ChadoSearch;
 use Drupal\chado_search\ChadoSearch\Interfaces\ChadoSearchInterface;
 
 /**
- * ChadoSearch plugin manager.
+ * Manages ChadoSearch Plugin instances.
+ *
+ * Creates search interfaces based on the information provided by specific
+ * plugin instances including managing the form and route.
  */
-final class ChadoSearchPluginManager extends DefaultPluginManager {
+final class ChadoSearchManager extends DefaultPluginManager {
 
   /**
-   * Constructs the object.
+   * Constructs the ChadoSearchManager object.
    */
   public function __construct(\Traversable $namespaces, CacheBackendInterface $cache_backend, ModuleHandlerInterface $module_handler) {
     parent::__construct(
