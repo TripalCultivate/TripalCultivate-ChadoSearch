021b453d18ea99684539edcbd621e355cdb10bf3
Lacey Sanderson <laceyannesanderson@gmail.com>
2024-12-13 18:04:50 +0000

    Start testing the basics of the base class.

---
+++ tests/src/Fixtures/ChadoSearchBasicallyBase.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Drupal\Tests\chado_search\Fixtures;
+
+use Drupal\chado_search\ChadoSearch\ChadoSearchPluginBase;
+
+/**
+ * Creates a fake plugin class to test that is essentially the base class.
+ *
+ *  @ChadoSearch(
+ *    id = "basically_base",
+ *    title = @Translation("Basically Base"),
+ *    description = @Translation("A Fake plugin instance to test the base plugin class."),
+ *    permissions = {"access content"},
+ *    url_path = "search-fakers",
+ *    button_text = @Translation("Search"),
+ *    require_submit = TRUE,
+ *    pager = TRUE,
+ *    num_items_per_page = 25,
+ *  )
+ */
+class ChadoSearchBasicallyBase extends ChadoSearchPluginBase {
+
+  /**
+   * Information regarding the fields and filters for this search.
+   *
+   * @var array
+   */
+  public static $info = [
+    // Lists the columns in your results table.
+    'fields' => [
+      'column1' => [
+        'title' => 'Column1',
+      ],
+      'column2' => [
+        'title' => 'Column2',
+      ],
+    ],
+    // The filter criteria available to the user.
+    // This is used to generate a search form which can be altered.
+    'filters' => [
+      'column1' => [
+        'title' => 'Column1',
+        'help' => 'The first filter.',
+      ],
+      'mom_name' => [
+        'title' => 'Column2',
+        'help' => 'The second filter.',
+      ],
+    ],
+  ];
+
+  /**
+   * The query method.
+   *
+   * @param string $query
+   *   The full SQL query to execute. This will be executed using chado_query()
+   *   so use curly brackets appropriately. Use :placeholders for any values.
+   * @param array $args
+   *   An array of arguments to pass to chado_query(). Keys must be the
+   *   placeholders in the query and values should be what you want them set to.
+   * @param int $offset
+   *   The number of records to offset for the results. This is used in paging.
+   */
+  public function getQuery(string &$query, array &$args, int $offset) {
+
+  }
+
+}
---
+++ tests/src/Kernel/ChadoSearchPlugin/PluginBaseTest.php
@@ -0,0 +1,169 @@
+<?php
+
+namespace Drupal\Tests\chado_search\Kernel\Validators;
+
+use Drupal\Tests\tripal_chado\Kernel\ChadoTestKernelBase;
+use Drupal\Tests\chado_search\Fixtures\ChadoSearchBasicallyBase;
+use Drupal\tripal_chado\Database\ChadoConnection;
+use Drupal\chado_search\Services\ChadoSearchManager;
+
+/**
+ * Tests ChadoSearch Plugin Base functions.
+ *
+ * @group chado_search
+ * @group chado_search_plugin
+ */
+class PluginBaseTest extends ChadoTestKernelBase {
+
+  /**
+   * The Validators plugin manager for creating new validator instances.
+   *
+   * @var \Drupal\chado_search\Services\ChadoSearchManager
+   */
+  protected ChadoSearchManager $plugin_manager;
+
+  /**
+   * A Database query interface for querying Chado using Tripal DBX.
+   *
+   * @var \Drupal\tripal_chado\Database\ChadoConnection
+   */
+  protected ChadoConnection $chado_connection;
+
+  /**
+   * Configuration.
+   *
+   * @var config_entity
+   */
+  private $config;
+
+  /**
+   * Modules to enable.
+   *
+   * @var array
+   */
+  protected static $modules = [
+    'tripal_chado',
+    'chado_search',
+  ];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp(): void {
+    parent::setUp();
+
+    // Set test environment.
+    \Drupal::state()->set('is_a_test_environment', TRUE);
+
+    // Install module configuration.
+    $this->installConfig(['chado_search']);
+
+    // Test Chado database.
+    // Create a test chado instance and then set it in the container for use by
+    // our service.
+    $this->chado_connection = $this->createTestSchema(ChadoTestKernelBase::PREPARE_TEST_CHADO);
+
+    // Set plugin manager service.
+    $this->plugin_manager = \Drupal::service('chado_search.manager');
+  }
+
+  /**
+   * Test the checkIndices() function in the Validator Base class.
+   */
+  public function testBaseAnnotationGetters() {
+
+    $expected_annotation = [
+      'id' => "basically_base",
+      'title' => "Basically Base",
+      'description' => "A Fake plugin instance to test the base plugin class.",
+      'permissions' => ["access content"],
+      'url_path' => "search-fakers",
+      'button_text' => "Search",
+      'require_submit' => TRUE,
+      'pager' => TRUE,
+      'num_items_per_page' => 25,
+    ];
+
+    $configuration = [];
+    $plugin_id = 'basically_base';
+    $plugin_definition = $expected_annotation;
+    $instance = new ChadoSearchBasicallyBase($configuration, $plugin_id, $plugin_definition, $this->chado_connection);
+    $this->assertIsObject(
+      $instance,
+      "Unable to create ChadoSearchBasicallyBase plugin instance to test the base class."
+    );
+
+    // ID.
+    $returned_value = $instance->id();
+    $this->assertEquals(
+      $expected_annotation['id'],
+      $returned_value,
+      "The ID did not match what we expected based on the annotation set for ChadoSearchBasicallyBase.",
+    );
+
+    // Title.
+    $returned_value = $instance->title();
+    $this->assertEquals(
+      $expected_annotation['title'],
+      $returned_value,
+      "The title did not match what we expected based on the annotation set for ChadoSearchBasicallyBase.",
+    );
+
+    // description.
+    $returned_value = $instance->description();
+    $this->assertEquals(
+      $expected_annotation['description'],
+      $returned_value,
+      "The description did not match what we expected based on the annotation set for ChadoSearchBasicallyBase.",
+    );
+
+    // permissions.
+    $returned_value = $instance->permissions();
+    $this->assertEquals(
+      $expected_annotation['permissions'],
+      $returned_value,
+      "The permissions array did not match what we expected based on the annotation set for ChadoSearchBasicallyBase.",
+    );
+
+    // URL Path.
+    $returned_value = $instance->urlPath();
+    $this->assertEquals(
+      $expected_annotation['url_path'],
+      $returned_value,
+      "The URL path did not match what we expected based on the annotation set for ChadoSearchBasicallyBase.",
+    );
+
+    // Button Text.
+    $returned_value = $instance->buttonText();
+    $this->assertEquals(
+      $expected_annotation['button_text'],
+      $returned_value,
+      "The Button Text did not match what we expected based on the annotation set for ChadoSearchBasicallyBase.",
+    );
+
+    // Require Submit.
+    $returned_value = $instance->requireSubmit();
+    $this->assertEquals(
+      $expected_annotation['require_submit'],
+      $returned_value,
+      "The require submit did not match what we expected based on the annotation set for ChadoSearchBasicallyBase.",
+    );
+
+    // Pager.
+    $returned_value = $instance->usePager();
+    $this->assertEquals(
+      $expected_annotation['pager'],
+      $returned_value,
+      "The pager did not match what we expected based on the annotation set for ChadoSearchBasicallyBase.",
+    );
+
+    // Number of items per page.
+    $returned_value = $instance->numItemsPerPage();
+    $this->assertEquals(
+      $expected_annotation['num_items_per_page'],
+      $returned_value,
+      "The Number of items per page did not match what we expected based on the annotation set for ChadoSearchBasicallyBase.",
+    );
+  }
+
+}
