710532f6246974892fe7f28515b2a8644736dfa0
Lacey-Anne Sanderson <las166@mail.usask.ca>
2024-12-14 03:54:29 +0000

    Lay a lot of the groundwork for the test.

--- tests/src/Kernel/ChadoSearchPlugin/FormBaseTest.php
+++ tests/src/Kernel/ChadoSearchPlugin/FormBaseTest.php
@@ -5,8 +5,10 @@ namespace Drupal\Tests\chado_search\Kernel\Validators;
 use Drupal\chado_search\ChadoSearch\Interfaces\ChadoSearchInterface;
 use Drupal\chado_search\Services\ChadoSearchManager;
 use Drupal\Tests\tripal_chado\Kernel\ChadoTestKernelBase;
-use Drupal\Tests\chado_search\Fixtures\ChadoSearchBasicallyBase;
 use Drupal\tripal_chado\Database\ChadoConnection;
+use Symfony\Component\HttpFoundation\InputBag;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
 
 /**
  * Tests ChadoSearch Plugin Base functions.
@@ -42,6 +44,49 @@ class FormBaseTest extends ChadoTestKernelBase {
   ];
 
   /**
+   * Plugin definitions for various fake plugin instances used for testing.
+   *
+   * @var array
+   *   A list of fake plugin instances where the key is the if of the instance
+   *   and the value is the plugin definition to be based in when creating it.
+   */
+  protected array $plugin_definitions = [
+    'basically_base' => [
+      'id' => "basically_base",
+      'title' => "Basically Base",
+      'description' => "A Fake plugin instance to test the base plugin class.",
+      'permissions' => ["access content"],
+      'url_path' => "search-fakers",
+      'button_text' => "Search",
+      'require_submit' => TRUE,
+      'pager' => TRUE,
+      'num_items_per_page' => 25,
+    ],
+    'less_than_1page' => [
+      'id' => "less_than_1page",
+      'title' => "Search Less than one page",
+      'description' => "A Fake plugin instance to test the base plugin class.",
+      'permissions' => ["access content"],
+      'url_path' => "search-fakers",
+      'button_text' => "Search",
+      'require_submit' => TRUE,
+      'pager' => TRUE,
+      'num_items_per_page' => 6,
+    ],
+    'exactly2pages' => [
+      'id' => "exactly2pages",
+      'title' => "Search exactly 2 pages",
+      'description' => "A Fake plugin instance to test the base plugin class.",
+      'permissions' => ["access content"],
+      'url_path' => "search-fakers",
+      'button_text' => "Search",
+      'require_submit' => TRUE,
+      'pager' => TRUE,
+      'num_items_per_page' => 3,
+    ],
+  ];
+
+  /**
    * {@inheritdoc}
    */
   protected function setUp(): void {
@@ -55,20 +100,18 @@ class FormBaseTest extends ChadoTestKernelBase {
     // our service.
     $this->chado_connection = $this->createTestSchema(ChadoTestKernelBase::PREPARE_TEST_CHADO);
 
+  }
+
+  /**
+   * HELPER: Creates the instance and sets the plugin manager to return it.
+   */
+  protected function setUpPluginInstance($instance_info) {
+
     $configuration = [];
-    $plugin_id = 'basically_base';
-    $plugin_definition = [
-      'id' => "basically_base",
-      'title' => "Basically Base",
-      'description' => "A Fake plugin instance to test the base plugin class.",
-      'permissions' => ["access content"],
-      'url_path' => "search-fakers",
-      'button_text' => "Search",
-      'require_submit' => TRUE,
-      'pager' => TRUE,
-      'num_items_per_page' => 25,
-    ];
-    $this->search_instance = new ChadoSearchBasicallyBase(
+    $plugin_id = $instance_info['id'];
+    $plugin_definition = $this->plugin_definitions[$plugin_id];
+    $plugin_class = '\Drupal\Tests\chado_search\Fixtures\\' . $instance_info['class'];
+    $this->search_instance = new $plugin_class(
       $configuration,
       $plugin_id,
       $plugin_definition,
@@ -83,9 +126,114 @@ class FormBaseTest extends ChadoTestKernelBase {
   }
 
   /**
-   * Tests the form without worrying about the query being performed.
+   * HELPER: Sets up mocks for the query params of the current request.
    */
-  public function testForm() {
+  protected function setUpUrlQueryParams($query_params) {
+
+    // InputBag.
+    $query = new InputBag(
+      $query_params,
+    );
+    $post = new InputBag([]);
+
+    // Current request.
+    $current_request = $this->createMock(Request::class);
+    $current_request->query = $query;
+    $current_request->request = $post;
+
+    // Request Stack.
+    $request_stack = $this->createMock(RequestStack::class);
+    $request_stack->method('getCurrentRequest')
+      ->willReturn($current_request);
+    $this->container->set('request_stack', $request_stack);
+  }
+
+  /**
+   * Data Provider: provides fake instances and associated expectations.
+   *
+   * @return array
+   *   An array of scenarios.
+   */
+  public static function provideChadoSearchScenarios() {
+    $scenario = [];
+
+    // First lets define the instance info to be used for multiple scenarios.
+    $instance_info = [];
+    $instance_info['basically_base'] = [
+      'id' => 'basically_base',
+      'class' => 'ChadoSearchBasicallyBase',
+    ];
+    $instance_info['less_than_1page'] = [
+      'id' => 'less_than_1page',
+      'class' => 'ChadoSearchLess1Page',
+    ];
+    $instance_info['exactly2pages'] = [
+      'id' => 'exactly2pages',
+      'class' => 'ChadoSearchExactly2Pages',
+    ];
+
+    // #0
+    // Basic test ignoring the query and as close to the base class as possible.
+    $scenario[] = [
+      $instance_info['basically_base'],
+      [
+        'offset' => 0,
+        'page_num' => 1,
+      ],
+    ];
+
+    // #1
+    // Ignoring the query, returns 3 results (<1 page), check the first page.
+    $scenario[] = [
+      $instance_info['less_than_1page'],
+      [
+        'offset' => 0,
+        'page_num' => 1,
+      ],
+    ];
+
+    // #2
+    // Ignoring the query, returns 3 results (<1 page), check the second page.
+    $scenario[] = [
+      $instance_info['less_than_1page'],
+      [
+        'offset' => 6,
+        'page_num' => 2,
+      ],
+    ];
+
+    // #3
+    // Ignoring the query, returns 6 results (=2 page3), check the first page.
+    $scenario[] = [
+      $instance_info['exactly2pages'],
+      [
+        'offset' => 0,
+        'page_num' => 1,
+      ],
+    ];
+
+    // #4
+    // Ignoring the query, returns 6 results (=2 page3), check the second page.
+    $scenario[] = [
+      $instance_info['exactly2pages'],
+      [
+        'offset' => 3,
+        'page_num' => 2,
+      ],
+    ];
+
+    return $scenario;
+  }
+
+  /**
+   * Tests the form: no querying, no results.
+   *
+   * @dataProvider provideChadoSearchScenarios
+   */
+  public function testForm($instance_info, $query_params) {
+
+    $this->setUpPluginInstance($instance_info);
+    $this->setUpURLQueryParams($query_params);
 
     // Build the form using the Drupal form builder.
     $form = \Drupal::formBuilder()->getForm(
